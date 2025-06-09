e5802b58c154ceea51c77b05464f26563baf4429
Luffy-456 <anshuman567.in@gmail.com>
2025-06-09 17:55:05 +0530

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd ..
 echo "removing unnecessary dir & files from vitmain to build aosp "
 rm -rf kernel/common/android12-5.10/include/kunit/fff.h
 rm -rf  kernel/common/android12-5.10/include/soc/oplus/boot
