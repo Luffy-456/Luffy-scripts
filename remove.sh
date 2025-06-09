80bd438968a010d946a21538ead74e1b64d8b08d
Luffy-456 <anshuman567.in@gmail.com>
2025-06-09 18:06:16 +0530

    added support to remove more not required files

--- remove.sh
+++ remove.sh
@@ -6,3 +6,5 @@ rm -rf  kernel/common/android12-5.10/include/soc/oplus/boot
 rm -rf  kernel/common/android12-5.10/include/soc/oplus/dfr
 rm -rf  kernel/common/android12-5.10/include/soc/oplus/dft
 rm -rf kernel/common/android12-5.10/include/soc/oplus/system/oplus_mm_kevent_fb.h
+rm -rf hardware/oplus/hidl/powershare
+rm -rf livedisplay
