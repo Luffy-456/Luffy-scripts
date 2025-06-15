4fb3672c0c44499197929dd7d5c0acb5a4f6e9bb
Luffy-456 <anshuman567.in@gmail.com>
2025-06-15 22:43:09 +0530

    updated scripts

---
+++ clone_extra_apps.sh
@@ -0,0 +1,11 @@
+echo "Cloning extra apps : BCR , ViPER4AndroidFX "
+
+echo "Started cloning extra apps "
+
+echo "Cloning BCR"
+git clone https://github.com/Evolution-X-AOSP/vendor_bcr -b udc vendor/bcr
+
+echo "Cloning Viper4Android"
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
+
+echo "Done cloning extra apps "
